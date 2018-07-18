5548e1306bd2247bf417cadb67a0b0ee977fd8c7
GullipalliVijayaAditya <vijayaaditya1996@gmail.com>
2018-07-18 10:23:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/gabwithus/models/Friend.java
@@ -0,0 +1,22 @@
+package com.example.android.gabwithus.models;
+
+
+public class Friend {
+    private long date;
+
+    public Friend() {
+
+    }
+
+    public Friend(long date) {
+        this.date = date;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+}
